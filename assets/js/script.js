16ef36fd7a70649c5ac920728fde5b97fbd7a524
Emmanuel Nggadas <enggadas80@gmail.com>
2017-04-18 00:51:34 +0100

    places near you

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,10 @@
 
 function activitiesModal() {
     // Get the modal
-    var modal = document.getElementById('modalActivity');
+    var modal = document.getElementById('activityModal');
 
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    var span = document.getElementsByClassName("close-activity")[0];
 
     openModal();
 
@@ -27,4 +27,95 @@ function activitiesModal() {
             modal.style.display = "none";
         }
     }
+}
+
+function placesModal() {
+    // Get the modal
+    var modal = document.getElementById('placesModal');
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close-places")[0];
+
+    openModal();
+
+    // When the user clicks the button, open the modal
+    function openModal() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+function groupsModal() {
+    // Get the modal
+    var modal = document.getElementById('groupModal');
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close-group")[0];
+
+    openModal();
+
+    // When the user clicks the button, open the modal
+    function openModal() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+function peopleModal() {
+    // Get the modal
+    var modal = document.getElementById('peopleModal');
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close-people")[0];
+
+    openModal();
+
+    // When the user clicks the button, open the modal
+    function openModal() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+function checkMark() {
+    document.getElementsByClassName('add-btn').innerHTML = '&#10004;';
+}
+
+function feedbackAlert() {
+
+    alert("Your request has been sent, You will be notified with an update.");
+
 }
