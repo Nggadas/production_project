b8d711e2d6bb69e483f505eb511cfca3c35121b4
Emmanuel Nggadas <enggadas80@gmail.com>
2017-04-18 10:41:40 +0100

    dropdown for activities page

---
+++ assets/js/dropdown.js
@@ -0,0 +1,24 @@
+/**
+ * Created by nggadas on 18/04/17.
+ */
+
+/* When the user clicks on the button,
+ toggle between hiding and showing the dropdown content */
+function placesDropdown() {
+    document.getElementById("placesDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
